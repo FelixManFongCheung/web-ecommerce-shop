217bd402468016b2a6cead23d4225e15c9e9c68d
FelixManFongCheung <manfongcheung74@gmail.com>
2024-09-24 15:50:23 +0200

    css installed

---
+++ src/app/products/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import styles from './products.module.scss';
+import { clsx } from 'clsx'
+import { CSSProperties, useState } from 'react';
+
+export default function Page() {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const imageArray: string[] = ['https://placehold.co/600x400/orange/white', 'https://placehold.co/600x400/white/black', 'https://placehold.co/600x400/black/white', 'https://placehold.co/600x400/pink/white', 'https://placehold.co/600x400/yellow/white'];
+
+  function prev(): void {
+    if (imageIndex > 0) setImageIndex(imageIndex - 1)
+  }
+  
+  function next(): void {
+    if (imageIndex < imageArray.length - 1) {
+      setImageIndex(imageIndex + 1);
+    }
+  }
+  
+  return (
+    <>
+    <div className={styles['products-page']}>
+      {imageArray.map((url) => (
+        <img key={url} src={url} className={styles['image']} alt="" style={{
+          translate: `${imageIndex*-100}%`,
+        }} />
+      ))}
+      <button onClick={prev} className={clsx(styles.arrow, styles.prev)}></button>
+      <button onClick={next} className={clsx(styles.arrow, styles.next)}></button>
+    </div>
+    <div className={styles['pagiantion']}>
+      {imageArray.map((url,index) => (
+        <button style={index == imageIndex ? {all: 'unset', scale: '1.5', transition: 'scale 0.5s ease', cursor: 'pointer'} : {all: 'unset', cursor: 'pointer'}} onClick={()=> {
+          setImageIndex(index)
+        }} key={url}>{index}</button>
+      ))}
+    </div>
+    </>
+  )
+};
