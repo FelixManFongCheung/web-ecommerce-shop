e7ce6533c9aee7f51c9c2b81c302204658d94bdb
FelixManFongCheung <manfongcheung74@gmail.com>
2024-09-25 13:08:42 +0200

    rotating products done

--- src/app/products/product/page.tsx
+++ src/app/products/product/page.tsx
@@ -4,11 +4,15 @@ import clsx from 'clsx';
 export default function Product({url}: {url:string}) {  
   return (
     <a className={styles['image-wrapper']} href="">
-      <div className={clsx(styles.product, styles['product-front'])}>
-        <img src={url} alt="pic" />
+      <div className={styles['test-front']}>
+        <div className={clsx(styles.product, styles['product-front'])}>
+          <img src={url} alt="pic" />
+        </div>
       </div>
-      <div className={clsx(styles.product, styles['product-back'])}>
-        <img src={url} alt="pic" />
+      <div className={styles['test-back']}>
+        <div className={clsx(styles.product, styles['product-back'])}>
+          <img src={url} alt="pic" />
+        </div>
       </div>
     </a>
   )
