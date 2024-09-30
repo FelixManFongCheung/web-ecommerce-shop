63315481ccebe5fb378c10dafbbc0aa1a1420378
FelixManFongCheung <manfongcheung74@gmail.com>
2024-09-30 17:03:06 +0200

    useclient fix

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,4 +1,4 @@
-'use clinet'
+'use client'
 
 import Link from 'next/link';
 import styles from './header.module.scss';
--- src/app/components/mobileNav/index.tsx
+++ src/app/components/mobileNav/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import styles from './mobilenav.module.scss';
 import Link from 'next/link';
 import { ActionTypes } from '@/actions';
