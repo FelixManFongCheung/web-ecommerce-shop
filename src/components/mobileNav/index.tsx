431a1216b6211e956dd37d3e53e7117bd844e791
FelixManFongCheung <manfongcheung74@gmail.com>
2024-11-28 15:39:24 +0100

    mobile function init

--- src/components/mobileNav/index.tsx
+++ src/components/mobileNav/index.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import styles from './mobilenav.module.scss';
-import Link from 'next/link';
 import clsx from 'clsx';
 import useAppStore from '@/stores';
+import TabsWrapper from '../tabs';
 
 export default function MobileNav() {
   const store = useAppStore();
@@ -12,17 +12,7 @@ export default function MobileNav() {
     <div className={clsx(styles['mobile-nav-wrapper'], store.isOpen && styles.open)}>
       <div className={styles['mobile-nav-backdrop']} onClick={store.toggleOpen}></div>
       <div className={styles['mobile-nav-content']}>
-        <ul>
-            <li>
-              <Link href='/products'>Products</Link>
-            </li>
-            <li>
-              <Link href='/products'>Products</Link>
-            </li>
-            <li>
-              <Link href='/products'>Products</Link>
-            </li>
-        </ul>
+        <TabsWrapper isMobile={true} />
       </div>
     </div>
   )
