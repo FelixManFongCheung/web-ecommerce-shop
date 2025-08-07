dbd1081ad2232e775a15241ef2239046f169d028
FelixManFongCheung <manfongcheung74@gmail.com>
2025-08-07 14:33:43 +0200

    ordered component exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,20 +1,26 @@
 import ATC from "./atc";
 import CartPopup from "./cartPopup";
 import { Checkout, CheckoutButton } from "./checkout";
-import DecoratorLines from "./decoratorLines";
+
+// main UI components
 import Filter from "./filter";
 import Footer from "./footer";
 import Header from "./header";
 import Hero from "./hero";
-import Icon from "./icon";
 import MobileNav from "./mobileNav";
+import TabsWrapper from "./tabs";
+
+// UI units
 import ModalProductCard from "./modalProductCard";
 import ModalWrapper from "./modalWrapper";
 import ProductCard from "./productCard";
 import ProductCardSkeleton from "./productSkeleton";
 import RemoveItem from "./removeItem";
+
+// custom UI units
+import DecoratorLines from "./decoratorLines";
+import Icon from "./icon";
 import SpinningProductCard from "./spinningProductCard";
-import TabsWrapper from "./tabs";
 
 export {
   ATC,
