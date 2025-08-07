885cfa4a76c4612c5963b5364fe4eb87e8a7d6df
FelixManFongCheung <manfongcheung74@gmail.com>
2025-08-07 13:50:13 +0200

    added asset and prelim footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,58 +1,34 @@
+import { Icon } from "@/components";
+import { Instagram, Mail } from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="bottom-0 h-fit w-full bg-white px-5 pb-12 grid grid-cols-3 gap-20 md:flex md:flex-col md:gap-8">
-      <div className="md:order-3">
-        <p>Proxy Archive</p>
-        <br />
-        <p>
-          Your gateway to exquisite vintage garments. We preserve pieces of
-          history and showcase excellent quality, hoping to store an enduring
-          sense of affection on your sentimental harddrive.
-        </p>
-        <br />
-        <a
-          href="https://example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className=""
-        >
-          @proxy__archive
-        </a>
-        <br />
-        <br />
-        <p>©{new Date().getFullYear()}</p>
+    <div className="container mx-auto flex justify-between bottom-0 h-fit w-full pb-10 bg-white">
+      <div className="flex flex-col gap-4 w-[200px]">
+        <Icon />
+        <Link className="flex flex-row gap-2" href="/">
+          <Mail />
+          <span>info@proxyarchive.com</span>
+        </Link>
+        <Link className="flex flex-row gap-2" href="/">
+          <Instagram />
+          <span>@proxy__archive</span>
+        </Link>
       </div>
-      <div className="md:order-1">
-        <p>Menu</p>
-        <br />
-        <div className="flex flex-col gap-4">
-          <Link href="/contact">Contact us</Link>
-          <Link href="/">Shipping</Link>
-          <Link href="/">Return Policy</Link>
-          <Link href="/">Terms and conditions</Link>
-        </div>
-      </div>
-      <div className="md:order-2">
-        <strong>Never Miss a Drop!</strong>
-        <br />
-        <br />
-        <p>
-          Sign up to receive exclusive restock updates, discounts and
-          promotions.
-        </p>
-        <br />
-        <div className="">
-          <input
-            type="email"
-            placeholder="Enter you e-mail address"
-            className="flex flex-col gap-4 rounded-none border border-gray-400 mb-4 p-2 w-full placeholder:text-black"
-          />
-          <button className="bg-black text-lg text-white py-2 px-10 w-fit">
-            Subscribe
-          </button>
-        </div>
+      <div className="flex flex-col gap-4 w-[200px]">
+        <span className="text-primary text-right text-wrap">
+          JOIN THE PROXY PAPER Sign up for exclusive restock updates, pre-sale
+          access, and curated promotions.
+        </span>
+        <input
+          type="email"
+          placeholder="enter your email address here"
+          className="w-full border-2 border-primary p-1"
+        />
+        <button className="bg-primary text-white max-w-fit p-1 self-end">
+          SUBSCRIBE
+        </button>
       </div>
     </div>
   );
