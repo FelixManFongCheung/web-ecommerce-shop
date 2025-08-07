825f85597d9ac7a3b9947ecb70e008c970ae9972
FelixManFongCheung <manfongcheung74@gmail.com>
2025-08-07 14:45:14 +0200

    aligned icon in mobile nav with header icon

--- src/components/mobileNav/index.tsx
+++ src/components/mobileNav/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Icon } from "@/components";
 import { cn } from "@/lib/utils";
 import { useAppActions, useIsOpen } from "@/stores";
 
@@ -10,13 +11,13 @@ export default function MobileNav() {
   return (
     <div
       className={cn(
-        "block opacity-0 fixed top-0 left-0 w-full h-screen overflow-hidden transition-opacity duration-500 ease-in-out z-20 pointer-events-none",
+        "block opacity-0 fixed inset-0 w-full h-screen overflow-hidden transition-opacity duration-500 ease-in-out z-20 pointer-events-none",
         isOpen && "opacity-100 pointer-events-auto"
       )}
     >
       <div
         className={cn(
-          "fixed top-0 left-0 w-full h-screen transition-colors duration-500 ease-in-out",
+          "fixed inset-0 w-full h-screen transition-colors duration-500 ease-in-out",
           isOpen ? "bg-black/70" : "bg-black/0"
         )}
         onClick={toggleOpen}
@@ -26,7 +27,9 @@ export default function MobileNav() {
           "fixed bg-white mx-auto left-[50%] translate-x-[-50%] px-4 w-1/2 h-screen flex flex-col gap-5 transition-all duration-500 ease-in-out",
           isOpen ? "block" : "hidden pointer-events-none"
         )}
-      ></div>
+      >
+        <Icon className={cn("h-header-height mx-auto justify-center")} />
+      </div>
     </div>
   );
 }
