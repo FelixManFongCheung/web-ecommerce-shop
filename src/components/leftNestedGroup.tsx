c51eabe633bfcf7160744cf4bc6d009ab54b87a4
FelixManFongCheung <manfongcheung74@gmail.com>
2025-08-12 15:56:02 +0200

    completed categories

--- src/components/leftNestedGroup.tsx
+++ src/components/leftNestedGroup.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { type Group } from "@/components/desktopLeftNav";
 import { useState } from "react";
 
@@ -9,22 +10,38 @@ export default function LeftNestedGroup({
   group: Group;
   level?: number;
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(!isOpen);
+  const [openItems, setOpenItems] = useState<Set<string>>(new Set());
+
+  const handleClick = (key: string) => {
+    setOpenItems((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(key)) {
+        newSet.delete(key);
+      } else {
+        newSet.add(key);
+      }
+      return newSet;
+    });
   };
+
   return Object.entries(group).map(([key, value]) => (
-    <div
-      onClick={handleClick}
-      key={key}
-      className={`ml-${level * 4} ${level !== 0 && "h-0 overflow-hidden"} ${
-        isOpen && "h-auto transition-all duration-300"
-      }`}
-    >
-      <h1 className={level === 0 ? "text-lg font-bold" : "text-sm font-medium"}>
+    <div key={key} style={{ marginLeft: `${level * 1}rem` }}>
+      <h1
+        className={level === 0 ? "text-lg font-bold" : "text-sm font-medium"}
+        onClick={() => handleClick(key)}
+        style={{ cursor: "pointer" }}
+      >
         {key}
       </h1>
-      {value && LeftNestedGroup({ group: value as Group, level: level + 1 })}
+      {value && (
+        <div
+          className={`${
+            openItems.has(key) ? "h-auto" : "h-0"
+          } overflow-hidden transition-all duration-300`}
+        >
+          <LeftNestedGroup group={value as Group} level={level + 1} />
+        </div>
+      )}
     </div>
   ));
 }
