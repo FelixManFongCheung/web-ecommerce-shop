a6ac5a0e08a188c325a1813d8000cea5255d0f2a
FelixManFongCheung <manfongcheung74@gmail.com>
2025-08-13 16:27:10 +0200

    grid working for menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -10,28 +10,32 @@ export default function Menu({ style }: { style?: React.CSSProperties }) {
   return (
     <div
       className={cn(
-        "w-screen h-fit bg-primary text-secondary grid-col-1 md:grid-cols-4 fixed z-12",
+        "w-screen h-fit bg-primary text-secondary fixed z-12",
         isOpen ? "block" : "hidden"
       )}
       style={style}
     >
-      {/* format */}
-      <div className="flex flex-col gap-2">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      {/* panel */}
-      <div className="flex flex-col gap-2">
-        <div></div>
-      </div>
-      {/* info */}
-      <div className="flex flex-col gap-2">
-        {menuConfig.info.map((item) => (
-          <Link key={`menu-${item.label}`} href={item.href}>
-            {item.label}
-          </Link>
-        ))}
+      <div className="relative w-full grid grid-cols-1 md:grid-cols-4">
+        {/* format */}
+        <div className="flex flex-col gap-2">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        {/* panel */}
+        <div className="flex flex-col gap-2">
+          <div></div>
+        </div>
+        {/* info */}
+        <div className="flex flex-col gap-2">
+          {menuConfig.info.map((item) => (
+            <Link key={`menu-${item.label}`} href={item.href}>
+              {item.label}
+            </Link>
+          ))}
+        </div>
+        {/* empty column for spacing */}
+        <div className="hidden md:block"></div>
       </div>
     </div>
   );
