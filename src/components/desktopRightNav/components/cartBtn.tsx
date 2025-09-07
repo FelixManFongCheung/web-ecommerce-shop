1aa5e9802637144eb8bff1a6bc18b244ae1509cf
FelixManFongCheung <manfongcheung74@gmail.com>
2025-09-07 14:44:46 +0200

    added cartcount

--- src/components/desktopRightNav/components/cartBtn.tsx
+++ src/components/desktopRightNav/components/cartBtn.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { getCartFromCookieClient } from "@/lib/cart/client";
 import { cn } from "@/lib/cn/utils";
 import { useAppActions } from "@/stores/appStore";
 
 export default function CartBtn({ className }: { className?: string }) {
   const { openCart } = useAppActions();
+  const cartCount = getCartFromCookieClient()?.products.length;
+
   return (
     <div onClick={openCart} className={cn("cursor-pointer", className)}>
-      <h1 className={cn("text-inherit text-wrap text-7xl")}>Cart</h1>
+      <h1 className={cn("text-inherit text-wrap text-7xl")}>
+        Cart({cartCount})
+      </h1>
     </div>
   );
 }
