e157f68575ac96bff0cccb62d3108b1aa5c0a245
FelixManFongCheung <manfongcheung74@gmail.com>
2025-09-17 12:23:10 +0200

    h-screen toh-fit for info pages

--- src/app/(behind-hero)/return/layout.tsx
+++ src/app/(behind-hero)/return/layout.tsx
@@ -2,7 +2,7 @@ import DesktopLeftNav from "@/components/desktopLeftNav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col p-4 gap-4 h-screen md:flex-row md:mt-header-height mt-header-height-mobile md:pl-desktop-info-pages-padding-left md:pr-desktop-info-pages-padding-right md:py-[100px]">
+    <div className="flex flex-col p-4 gap-4 h-fit md:flex-row md:mt-header-height mt-header-height-mobile md:pl-desktop-info-pages-padding-left md:pr-desktop-info-pages-padding-right md:py-[100px]">
       <DesktopLeftNav headerName="Confirmation"></DesktopLeftNav>
       {children}
     </div>
