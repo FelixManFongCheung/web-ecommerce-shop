906da8f0d27806677b4c1f3da92bcc09682e6537
FelixManFongCheung <manfongcheung74@gmail.com>
2025-09-17 11:41:50 +0200

    added getallproductimages server action

---
+++ src/actions/getProductImages/index.ts
@@ -0,0 +1,33 @@
+import { createClient } from "@/lib/supabase";
+
+export async function getAllProductImages(productId: string) {
+  const supabase = await createClient();
+  // TODO: remember to upload all images taken by Signe in the same image format
+  const { data, error } = await supabase.storage
+    .from("Products")
+    .list(productId);
+
+  if (error) {
+    console.error(error);
+    return [];
+  }
+
+  const imageUrls = await Promise.all(
+    data
+      .map(async (file) => {
+        const { data, error } = await supabase.storage
+          .from("Products")
+          .createSignedUrl(`${productId}/${file.name}`, 3600 * 24 * 30);
+
+        if (error) {
+          console.error(error);
+          return null;
+        }
+
+        return data.signedUrl;
+      })
+      .filter((url) => url !== null)
+  );
+
+  return imageUrls;
+}
