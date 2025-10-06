8ee46f28c9202468736bb127fc8ce910387cb266
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-06 13:52:15 +0200

    fixing header in mobile under desktopleftnav

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       {/* header z index 11 */}
       <div
         className={cn(
-          "fixed top-0 md:h-header-height h-header-height-mobile flex z-11 w-full justify-between px-5",
+          "fixed top-0 md:h-header-height h-header-height-mobile flex z-12 md:z-11 w-full justify-between px-5",
           "bg-white cart-open:bg-primary",
           "cart-open:z-20"
         )}
