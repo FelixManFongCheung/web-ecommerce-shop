f8713ca67fb74a21c1b54fb0ad30adfab577dc9d
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-09 22:00:48 +0200

    font-size update

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -15,7 +15,11 @@ export default function Icon({
           className={cn(
             "text-[1.2rem] md:text-3xl after:content-['*'] after:text-xl after:relative after:-top-1 md:after:-top-2 !font-[Happy_Times_Italic]",
             fontSize &&
-              `text-[${fontSize}px] after:text-[${fontSize - 8}px] after:-top-1`
+              `text-[${fontSize}px] md:text-[${fontSize}px] after:text-[${
+                fontSize - 8
+              }px] md:after:text-[${
+                fontSize - 8
+              }px] after:-top-1 md:after:-top-1`
           )}
         >
           proxy archive
