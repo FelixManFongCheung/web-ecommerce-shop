23af6c1baf368a436dfd161946b5ebd3d3006bb8
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-09 16:19:25 +0200

    font-size downsize for mobile icon

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -13,7 +13,7 @@ export default function Icon({
       <Link href="/">
         <div
           className={cn(
-            "text-3xl after:content-['*'] after:text-xl after:relative after:-top-2 !font-[Happy_Times_Italic]",
+            "text-[1.2rem] md:text-3xl after:content-['*'] after:text-xl after:relative after:-top-2 !font-[Happy_Times_Italic]",
             fontSize &&
               `text-[${fontSize}px] after:text-[${fontSize - 8}px] after:-top-1`
           )}
