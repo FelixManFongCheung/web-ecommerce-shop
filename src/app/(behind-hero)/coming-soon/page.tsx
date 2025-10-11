ede786094dcb23575e459b1d2d4fbea97fe57dc8
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-11 21:18:11 +0200

    temp facade for webpage

---
+++ src/app/(behind-hero)/coming-soon/page.tsx
@@ -0,0 +1,7 @@
+export default function ComingSoon() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center !font-[Happy_Times_Italic] text-4xl text-secondary bg-primary after:content-['*'] after:text-4xl after:relative after:-top-1">
+      Coming Soon
+    </div>
+  );
+}
