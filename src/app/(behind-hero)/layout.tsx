2a3609b02f043337c25fa6d020255b9936165509
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-13 11:33:52 +0200

    init for normal production withotu coming soon

--- src/app/(behind-hero)/layout.tsx
+++ src/app/(behind-hero)/layout.tsx
@@ -1,39 +1,39 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-// import { getProductsAll } from "@/actions/stripe";
-// import {
-//   CartPopup,
-//   CookieWrapper,
-//   DesktopRightNav,
-//   FollowUsDialog,
-//   Footer,
-//   Header,
-//   Nav,
-// } from "@/components";
-// import DynamicDesktopLeftNav from "@/components/desktopLeftNav/DynamicDesktopLeftNav";
-// import { getRecursiveFolder } from "@/components/desktopLeftNav/hooks";
+import { getProductsAll } from "@/actions/stripe";
+import {
+  CartPopup,
+  CookieWrapper,
+  DesktopRightNav,
+  FollowUsDialog,
+  Footer,
+  Header,
+  Nav,
+} from "@/components";
+import DynamicDesktopLeftNav from "@/components/desktopLeftNav/DynamicDesktopLeftNav";
+import { getRecursiveFolder } from "@/components/desktopLeftNav/hooks";
 
-// export default async function BehindHeroLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const productsAll = await getProductsAll();
-//   const groups = getRecursiveFolder(productsAll);
-//   return (
-//     <CookieWrapper className="relative">
-//       <Nav />
-//       <Header />
-//       <DesktopRightNav />
-//       <DynamicDesktopLeftNav groups={groups} />
-//       <div>{children}</div>
-//       <Footer />
-//       <CartPopup />
-//       <FollowUsDialog />
-//     </CookieWrapper>
-//   );
-// }
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+export default async function BehindHeroLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const productsAll = await getProductsAll();
+  const groups = getRecursiveFolder(productsAll);
+  return (
+    <CookieWrapper className="relative">
+      <Nav />
+      <Header />
+      <DesktopRightNav />
+      <DynamicDesktopLeftNav groups={groups} />
+      <div>{children}</div>
+      <Footer />
+      <CartPopup />
+      <FollowUsDialog />
+    </CookieWrapper>
+  );
 }
+
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return <div>{children}</div>;
+// }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,55 +1,55 @@
-// import { cancelOrder, completeOrder } from "@/actions/order";
-// import { retrieveSession } from "@/actions/stripe";
+import { cancelOrder, completeOrder } from "@/actions/order";
+import { retrieveSession } from "@/actions/stripe";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // const { pathname, searchParams } = request.nextUrl;
+  const { pathname, searchParams } = request.nextUrl;
 
-  if (request.nextUrl.pathname === "/coming-soon") {
-    return NextResponse.next();
-  }
-
-  return NextResponse.redirect(new URL("/coming-soon", request.url));
-
-  // if (pathname === "/return") {
-  //   const sessionId = searchParams.get("session_id");
-
-  //   if (!sessionId) {
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
-
-  //   try {
-  //     const session = await retrieveSession(sessionId);
-
-  //     if (session.status === "complete") {
-  //       await completeOrder(sessionId);
-
-  //       const url = new URL(request.url);
-  //       url.searchParams.set("status", "complete");
-
-  //       return NextResponse.rewrite(url);
-  //     }
-
-  //     if (session.status === "expired") {
-  //       await cancelOrder(sessionId);
-
-  //       return NextResponse.redirect(new URL("/collections/all", request.url));
-  //     }
-
-  //     if (session.status === "open") {
-  //       return NextResponse.redirect(new URL("/collections/all", request.url));
-  //     }
-  //   } catch (error) {
-  //     console.error("Error in middleware processing order:", error);
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
+  // if (request.nextUrl.pathname === "/coming-soon") {
+  //   return NextResponse.next();
   // }
 
-  // // Pass pathname through headers for server components
-  // const response = NextResponse.next();
-  // response.headers.set("x-pathname", pathname);
+  // return NextResponse.redirect(new URL("/coming-soon", request.url));
 
-  // return response;
+  if (pathname === "/return") {
+    const sessionId = searchParams.get("session_id");
+
+    if (!sessionId) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    try {
+      const session = await retrieveSession(sessionId);
+
+      if (session.status === "complete") {
+        await completeOrder(sessionId);
+
+        const url = new URL(request.url);
+        url.searchParams.set("status", "complete");
+
+        return NextResponse.rewrite(url);
+      }
+
+      if (session.status === "expired") {
+        await cancelOrder(sessionId);
+
+        return NextResponse.redirect(new URL("/collections/all", request.url));
+      }
+
+      if (session.status === "open") {
+        return NextResponse.redirect(new URL("/collections/all", request.url));
+      }
+    } catch (error) {
+      console.error("Error in middleware processing order:", error);
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  // Pass pathname through headers for server components
+  const response = NextResponse.next();
+  response.headers.set("x-pathname", pathname);
+
+  return response;
 }
 
 export const config = {
