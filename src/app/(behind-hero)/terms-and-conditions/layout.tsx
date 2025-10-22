2ddd649906da346b6189a2b6c82d6950b9017807
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-22 15:15:55 +0200

    content padding

--- src/app/(behind-hero)/terms-and-conditions/layout.tsx
+++ src/app/(behind-hero)/terms-and-conditions/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col p-4 gap-4 h-fit md:flex-row md:mt-header-height md:pl-desktop-info-pages-padding-left md:pr-desktop-info-pages-padding-right md:py-[var(--desktop-info-pages-padding-top)]">
+    <div className="flex flex-col p-4 px-[var(--mobile-content-padding-horizontal)] gap-4 h-fit md:flex-row md:mt-header-height md:pl-desktop-info-pages-padding-left md:pr-desktop-info-pages-padding-right md:py-[var(--desktop-info-pages-padding-top)]">
       {children}
     </div>
   );
