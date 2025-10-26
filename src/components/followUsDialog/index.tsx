ce513d83b975aef7b16ca7f4abcf60b3e0169b53
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-27 00:02:46 +0100

    added popup dialogue cookie and its expiry

--- src/components/followUsDialog/index.tsx
+++ src/components/followUsDialog/index.tsx
@@ -5,17 +5,47 @@ import { Dialog } from "@radix-ui/react-dialog";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
 import { DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+
+const COOKIE_NAME = "followUsDialogShown";
+const COOKIE_EXPIRY_DAYS = 1; // 24 hours
 
 export default function FollowUsDialog() {
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    setOpen(true);
+    // Check if cookie exists
+    const cookieValue = getCookie(COOKIE_NAME);
+    
+    // Only show dialog if cookie doesn't exist (i.e., user hasn't seen it recently)
+    if (!cookieValue) {
+      setOpen(true);
+    }
   }, []);
 
+  const handleOpenChange = (isOpen: boolean) => {
+    setOpen(isOpen);
+    
+    // When dialog is closed, set cookie with expiry date
+    if (!isOpen) {
+      const expiryDate = new Date();
+      expiryDate.setDate(expiryDate.getDate() + COOKIE_EXPIRY_DAYS);
+      
+      setCookie(COOKIE_NAME, "true", {
+        expires: expiryDate,
+        sameSite: "lax",
+      });
+    }
+  };
+
+  // Don't render anything if dialog shouldn't be shown
+  if (!open) {
+    return null;
+  }
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={handleOpenChange}>
       <DialogContent
         className={cn(
           "w-[80vw] md:w-[30vw] max-w-4xl container px-12! flex flex-col justify-center items-center text-secondary bg-primary rounded-none sm:rounded-none border-none pt-10 pb-0"
