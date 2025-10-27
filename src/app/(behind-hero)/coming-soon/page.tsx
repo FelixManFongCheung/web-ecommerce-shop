4287957ec7dcd2eb08e2aa231bfed12e112ec1af
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-27 12:41:03 +0100

    animation

--- src/app/(behind-hero)/coming-soon/page.tsx
+++ src/app/(behind-hero)/coming-soon/page.tsx
@@ -1,6 +1,6 @@
 export default function ComingSoon() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center !font-[Happy_Times_Italic] text-4xl text-secondary bg-primary after:content-['*'] after:text-4xl after:relative after:-top-1">
+    <div className="relative w-screen h-screen flex items-center justify-center !font-[Happy_Times_Italic] text-4xl text-secondary bg-primary before:content-['*'] before:animate-ping before:text-4xl before:absolute before:top-1/2 before:translate-y-[-50%] before:left-1/2 before:translate-x-[6.7rem] after:content-['*'] after:text-4xl after:absolute after:top-1/2 after:translate-y-[-50%] after:left-1/2 after:translate-x-[6.7rem]">
       Coming Soon
     </div>
   );
