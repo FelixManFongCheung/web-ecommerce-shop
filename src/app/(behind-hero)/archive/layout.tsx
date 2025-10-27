884fdde769ac2f24ca050982cb928457990e119b
FelixManFongCheung <manfongcheung74@gmail.com>
2025-10-27 11:20:47 +0100

    info pages updated

--- src/app/(behind-hero)/archive/layout.tsx
+++ src/app/(behind-hero)/archive/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="p-4 pt-0 md:py-header-height">{children}</div>;
+  return <div className="p-[var(--mobile-content-padding-horizontal)] pt-0 md:py-header-height">{children}</div>;
 }
--- src/app/(behind-hero)/terms-and-conditions/page.tsx
+++ src/app/(behind-hero)/terms-and-conditions/page.tsx
@@ -7,35 +7,35 @@ export default function TermsAndConditionsPage() {
           We are based in Denmark and ship worldwide. Orders are processed and
           shipped within 3–7 business days of purchase.
         </p>
-        <h3 className="text-xl mb-2">Shipping Rates</h3>
+        <h3 className="text-xl mb-2">SHIPPING RATES</h3>
         <ul className="ml-10 mb-4">
           <li>Denmark – free shipping</li>
           <li>EU – €25</li>
           <li>USA – €40</li>
           <li>Rest of the world – €40</li>
         </ul>
-        <h3 className="text-xl mb-4">Estimated Delivery Times</h3>
+        <h3 className="text-xl mb-4">ESTIMATED DELIVERY TIMES</h3>
         <div className="grid grid-cols-2 gap-4 mb-4">
-          <p>Denmark</p>
+          <p className="italic">Denmark</p>
           <p>typically within 1-2 business days after dispatch.</p>
-          <p>International</p>
+          <p className="italic">International</p>
           <p>typically within 3-7 business days after dispatch.</p>
         </div>
         {/* TODO: bold needs to be added for the font of this text */}
         <p className="mb-4">
-          <span className="font-extrabold italic !font-[Athelas_Bold_Italic]">
+          <span className="font-extrabold !font-[Athelas_Bold_Italic]">
             Please note
           </span>{" "}
-          – Proxy Archive is not responsible for any taxes or duties applied to
+          <span className="italic">– Proxy Archive is not responsible for any taxes or duties applied to
           international shipments. Lost or delayed packages must be addressed
           with the shipping provider, as we cannot be held liable once the order
-          has been dispatched.
+          has been dispatched. </span>
         </p>
       </div>
       <div className="flex-1">
         <h2 className="text-2xl mb-4">Returns</h2>
 
-        <p className="mb-4">
+        <p className="mb-4 italic">
           As a small, independent archive, Proxy Archive is unable to offer
           returns.
         </p>
